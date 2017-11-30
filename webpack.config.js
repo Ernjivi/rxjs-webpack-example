61970c36c01825f0241b94816de3e28d44c69a42
Ernesto Jiménez Villaseñor <ernesto@bedu.org>
2017-11-30 17:47:09 -0600

    Canged rule to process image to add svg

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
                 })
             },
             {
-                test: /\.(png|jpg|gif)$/,
+                test: /\.(png|jpg|gif|svg)$/,
                 use: [
                     {
                         loader: 'file-loader',
