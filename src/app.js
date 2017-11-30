27b57ea06410e6b922b1f4e77256d126ac26485d
Ernesto Jiménez Villaseñor <ernesto@bedu.org>
2017-11-30 13:27:30 -0600

    Added main style sheet

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import { map, switchMap } from "rxjs/Observable";
+import "./styles/main.scss";
 export default class SuperButton{
     constructor (selector){
         console.log(document.querySelector(selector));
