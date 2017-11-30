9dcaddec2f1dd5541bff4b23982c595db7572652
Ernesto Jiménez Villaseñor <ernesto@bedu.org>
2017-11-30 17:19:55 -0600

    BugFix: removed test line

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import 'bootstrap';
 import "./styles/main.scss";
 
-import { map, switchMap } from "rxjs/Observable";
-
 export default class SuperButton{
     constructor (selector){
         console.log(document.querySelector(selector));
